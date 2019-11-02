ee3695b4b8404cc40e2746ca889ce85b4ebd80e9
Nobleson <noblesity@gmail.com.com>
2019-11-02 23:16:28 +0100

    update

--- main.js
+++ main.js
@@ -29,20 +29,7 @@ next();
 
 
 var port = process.env.PORT || 8081;
-var localURI = "mongodb://localhost:27017/samplledb";
-var mongoURL = localURI;
 
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
 
 //All artisan routers
 //var indexRouter = require('./routes');
